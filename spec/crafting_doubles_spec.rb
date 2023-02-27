cbc80337d45ccaf3ccd1f9a623f0624ac925d7df
Paul Lazar <lazar_paul_2006@yahoo.com>
2023-02-27 20:04:26 +0000

    crafting doubles challenge

--- spec/crafting_doubles_spec.rb
+++ spec/crafting_doubles_spec.rb
@@ -1,7 +1,10 @@
 RSpec.describe "doubles challenge" do
   it "creates a sophisticated double" do
-    task_list = # ...
-    task = # ...
+    task_list = double :task_list, add: nil
+    task = double(:task)
+    expect(task_list).to receive(:list).with(no_args).and_return([task])
+    expect(task_list).to receive(:count).with(no_args).and_return(1)
+    expect(task_list).to receive(:clear).with(no_args).and_return(:success)
 
     # Don't edit below
     task_list.add(task)
